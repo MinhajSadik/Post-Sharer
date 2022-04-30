b9ddb44f4bbac17a0603b7bc776ce6acbc75dbf4
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-30 18:46:46 +0600

    [ADD] client Component Form Posts Post stracture

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,10 +1,15 @@
+import { AppBar, Container, Typography } from "@material-ui/core";
 import React from "react";
 
 function App() {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Post Sharer
+        </Typography>
+      </AppBar>
+    </Container>
   );
 }
 
